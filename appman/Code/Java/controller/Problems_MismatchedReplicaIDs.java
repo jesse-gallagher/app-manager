fade1432e32fa3b1714a8a1095c37fe8c79e60d4
Jesse Gallagher <jesse@iknowsomeguys.com>
2017-12-19 14:13:52 -0500

    Moved the known system paths exclusion for Mismatched Replica IDs from Java to the view

--- appman/Code/Java/controller/Problems_MismatchedReplicaIDs.java
+++ appman/Code/Java/controller/Problems_MismatchedReplicaIDs.java
@@ -1,9 +1,7 @@
 package controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.TreeSet;
 
 import util.BeanTabularListAdapter;
 
@@ -11,28 +9,10 @@ import frostillicus.controller.BasicXPageController;
 
 import lotus.domino.*;
 
-import com.ibm.commons.util.StringUtil;
 import com.ibm.xsp.extlib.util.ExtLibUtil;
 
 public class Problems_MismatchedReplicaIDs extends BasicXPageController {
 	private static final long serialVersionUID = 1L;
-	
-	private static final TreeSet<String> KNOWN_SYSTEM_PATHS;
-	static {
-		KNOWN_SYSTEM_PATHS = new TreeSet<String>(String.CASE_INSENSITIVE_ORDER);
-		KNOWN_SYSTEM_PATHS.addAll(Arrays.asList(
-			"certsrv.nsf",
-			"cppfbws.nsf",
-			"daoscat.nsf",
-			"dbdirman.nsf",
-			"doladmin.nsf",
-			"iNotes/Forms9.nsf",
-			"log.nsf",
-			"reports.nsf",
-			"schema.nsf",
-			"webadmin.nsf"
-		));
-	}
 
 	public BeanTabularListAdapter<model.Application> getApplications() throws NotesException {
 		List<model.Application> result = new ArrayList<model.Application>();
@@ -45,16 +25,12 @@ public class Problems_MismatchedReplicaIDs extends BasicXPageController {
 		ViewEntry categoryEntry = nav.getFirst();
 		while(categoryEntry != null) {
 			if(categoryEntry.isCategory() && categoryEntry.getChildCount() > 1) {
-				List<?> columnValues = categoryEntry.getColumnValues();
-				String filePath = StringUtil.toString(columnValues.get(0));
-				if(!KNOWN_SYSTEM_PATHS.contains(filePath)) {
-					ViewEntry databaseEntry = nav.getChild(categoryEntry);
-					while(databaseEntry != null) {
-						Document databaseDoc = databaseEntry.getDocument();
-						model.Application app = new model.Application(databaseDoc);
-						result.add(app);
-						databaseEntry = nav.getNextSibling(databaseEntry);
-					}
+				ViewEntry databaseEntry = nav.getChild(categoryEntry);
+				while(databaseEntry != null) {
+					Document databaseDoc = databaseEntry.getDocument();
+					model.Application app = new model.Application(databaseDoc);
+					result.add(app);
+					databaseEntry = nav.getNextSibling(databaseEntry);
 				}
 			}
 			categoryEntry = nav.getNextSibling(categoryEntry);
